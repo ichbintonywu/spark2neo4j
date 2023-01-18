96bcaa57772ac362f4e2957b2fec1a74599724a5
Tony Wu <48823951+ichbintonywu@users.noreply.github.com>
2023-01-18 16:19:50 +0800

    Update neo4jdemo_df_direct_into_neo4j.py

--- neo4jdemo_df_direct_into_neo4j.py
+++ neo4jdemo_df_direct_into_neo4j.py
@@ -1,29 +1,18 @@
 from pyspark.sql import SparkSession
 
-URI = "neo4j://localhost:11003"
-
 spark =(SparkSession.builder.master('local[*]')
         .appName('Leverage Neo4j')
-#        .config('spark.ui.port', '4050')
         .config('spark.sql.repl.eagerEval.enabled', True)
-        .config('spark.jars', '/Users/tonywu/Documents/dev/spring/testPyspark1/neo4j-connector-apache-spark_2.12-4.0.2_for_spark_3.jar')
+        .config('spark.jars', 'PathTo/neo4j-connector-apache-spark_2.12-4.0.2_for_spark_3.jar')
         .config("neo4j.url", URI).config("neo4j.authentication.type", "basic")
-        .config("neo4j.authentication.basic.username", 'neo4j')
-        .config("neo4j.authentication.basic.password", 'Neo4j!').getOrCreate())
+        .getOrCreate())
 
 # Create data in dataframe
-# data = [(('Ram'), '1991-04-01', 'M', 3000),
-#         (('Mike'), '2000-05-19', 'M', 4000),
-#         (('Rohini'), '1978-09-05', 'M', 4000),
-#         (('Maria'), '1967-12-01', 'F', 4000),
-#         (('Jenis'), '1980-02-17', 'F', 1200)]
-#columns = ["Name", "BirthDate", "Gender", "Salary"]
 data = [(('Ram'), '1991-04-01', 'M'),
         (('Mike'), '2000-05-19', 'M'),
         (('Rohini'), '1978-09-05', 'M'),
         (('Maria'), '1967-12-01', 'F',),
         (('Jenis'), '1980-02-17', 'F')]
-
 
 # Column names in dataframe
 columns = ["Name", "BirthDate", "Gender"]
@@ -33,8 +22,8 @@ df = spark.createDataFrame(data=data,
 
 # spark.read.format("org.neo4j.spark.DataSource") \
 #     .option("authentication.basic.username", "neo4j") \
-#     .option("authentication.basic.password", "Ne04j!") \
-#     .option("url", "bolt://localhost:11003") \
+#     .option("authentication.basic.password", "password") \
+#     .option("url", "bolt://localhost:7687") \
 #     .option("labels", ":Person") \
 #     .load()
 
@@ -42,8 +31,8 @@ df = spark.createDataFrame(data=data,
 # df.write.format("org.neo4j.spark.DataSource") \
 #     .mode("Append")\
 #     .option("authentication.basic.username", "neo4j") \
-#     .option("authentication.basic.password", "Ne04j!") \
-#     .option("url", "bolt://localhost:11003") \
+#     .option("authentication.basic.password", "password") \
+#     .option("url", "bolt://localhost:7687") \
 #     .option("labels", ":Person") \
 #     .save()
 
@@ -51,16 +40,16 @@ df = spark.createDataFrame(data=data,
 df.write.format("org.neo4j.spark.DataSource") \
     .mode("OverWrite") \
     .option("authentication.basic.username", "neo4j") \
-    .option("authentication.basic.password", "Ne04j!") \
-    .option("url", "bolt://localhost:11003") \
+    .option("authentication.basic.password", "password") \
+    .option("url", "bolt://localhost:7687") \
     .option("labels", ":Person") \
     .option("node.keys", "Name") \
     .save()
 
 data2 = [(('Ram'),  3000),
-        (('Mike'),  4000),
-        (('Rohini'),  4000),
-        (('Maria'), 4000),
+        (('Mike'), 4000),
+        (('Rohini'), 5000),
+        (('Maria'), 6000),
         (('Jenis'), 1200)]
 
 # Column names in dataframe
@@ -71,8 +60,8 @@ df2 = spark.createDataFrame(data=data2,
 df2.write.format("org.neo4j.spark.DataSource") \
     .mode("OverWrite") \
     .option("authentication.basic.username", "neo4j") \
-    .option("authentication.basic.password", "Ne04j!") \
-    .option("url", "bolt://localhost:11003") \
+    .option("authentication.basic.password", "password") \
+    .option("url", "bolt://localhost:7687") \
     .option("labels", ":Salary") \
     .option("node.keys", "Name") \
     .save()
@@ -81,8 +70,8 @@ df2.write.format("org.neo4j.spark.DataSource") \
 df.write.format("org.neo4j.spark.DataSource") \
     .mode("Append") \
     .option("authentication.basic.username", "neo4j") \
-    .option("authentication.basic.password", "Ne04j!") \
-    .option("url", "bolt://localhost:11003") \
+    .option("authentication.basic.password", "password") \
+    .option("url", "bolt://localhost:7687") \
     .option("relationship", "EARNED_SALARY") \
     .option("relationship.save.strategy", "keys") \
     .option("relationship.source.labels", ":Person") \
