d2f8057727ce82501b5d3b132aafdbedf45ed609
Tony Wu <48823951+ichbintonywu@users.noreply.github.com>
2023-01-29 11:27:57 +0800

    Update SimplSparkNeo4j.java

--- SimplSparkNeo4j.java
+++ SimplSparkNeo4j.java
@@ -1,11 +1,6 @@
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.SaveMode;
-import java.util.Arrays;
-import java.util.List;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
 
 public class SimplSparkNeo4j {
     public static void main(String[] args) {
@@ -16,9 +11,9 @@ public class SimplSparkNeo4j {
                 .getOrCreate();
         // Reading nodes from Neo4j
         Dataset<Row> ds = spark.read().format("org.neo4j.spark.DataSource")
-                .option("url", "bolt://localhost:11003")
+                .option("url", "bolt://localhost:7687")
                 .option("authentication.basic.username", "neo4j")
-                .option("authentication.basic.password", "Ne04j!")
+                .option("authentication.basic.password", "password")
                 .option("database", "neo4j")
                 .option("labels", "Person")
                 .load();
